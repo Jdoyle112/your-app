b9a29cdf653f55f9061f009ef0c822546cc93465
Jack <jdoyle112@gmail.comS>
2015-11-23 11:05:53 -0500

    debug user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,12 @@
 class UserMailer < ApplicationMailer
-	default from: "jdoyle112@gmail.com"
-end
+	default from: "from@example.com"
+
 
 def contact_form(email, name, message)
 	@message = message
 	mail(:from => email,
 		:to => "jdoyle112@gmail.com",
-		:subject => "A new contact form message")
+		:subject => "A new contact form message from #{name}")
 
 end 
 end
